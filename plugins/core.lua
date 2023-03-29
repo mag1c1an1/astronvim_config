4d1a953705f9dc2a5384d3dde11bbd08f924ea7d
mag1c1an1 <mag1cian@icloud.com>
2023-03-29 17:55:31 +0800

    <update>(snippets): add user snippets

--- plugins/core.lua
+++ plugins/core.lua
@@ -29,7 +29,7 @@ return {
     config = function(plugin, opts)
       require "plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
-      require("luasnip.loaders.from_vscode").lazy_load { paths = { "./user/snippets" } }
+      require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }
     end,
   },
   -- {
