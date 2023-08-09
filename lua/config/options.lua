3bf17ef598d2aaf55370b980a375b436fb29a0c7
mag1c1an1 <mag1cian@icloud.com>
2023-08-09 18:32:37 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.clipboard = {
+  name = "myClipboard",
+  copy = {
+    ["+"] = { "pbcopy" },
+    ["*"] = { "pbcopy" },
+  },
+  paste = {
+    ["+"] = { "pbpaste" },
+    ["*"] = { "pbpaste" },
+  },
+  cache_enabled = false,
+}
