a6f3d6f1964285d74769644993538f5fd09b9a5f
mag1c1an1 <mag1cian@icloud.com>
2023-08-09 16:11:56 +0800

    update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,7 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   build = ":Copilot auth",
+  enabled = false,
   opts = {
     suggestion = { enabled = true, auto_trigger = true },
     panel = { enabled = true },
